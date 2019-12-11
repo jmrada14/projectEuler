3cd692f37f4e00bbe6b762d3836f6f57ccbfdad9
jmrada14 <jmrada14@tutanota.com>
2019-12-10 19:28:37 -0500

    problem 2

--- multiples_of_3_and_5.rb
+++ multiples_of_3_and_5.rb
@@ -4,7 +4,7 @@
 #   If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these
 #  multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
-def multiples
+def solution
   array = (1..999).to_a
   new_array = Array.new
   solution = 0
@@ -17,4 +17,4 @@ def multiples
   puts(solution)
 end
 
-multiples
+solution
