3720cbda7cdf712912c693f867f6fde6ba6fc32b
jmrada14 <jmrada14@tutanota.com>
2019-12-13 13:44:52 -0500

    problem 10

---
+++ summation_of_primes.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+#  Project Euler exercises - Problem 10
+# Find the sum of all the primes below two million.
+
+require 'prime'
+
+sol = 0
+Prime.each(2_000_000) { |j| sol += j }
+p sol
